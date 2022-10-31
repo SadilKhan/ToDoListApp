a3992c42884639bd078fbd416bf1d0165a2fa335
SadilKhan <edwardstephendrood@gmail.com>
2022-10-31 12:48:28 +0100

    minor updates

--- ToDoListApp/ItemData/ItemDB.swift
+++ ToDoListApp/ItemData/ItemDB.swift
@@ -13,14 +13,14 @@ class ItemDB: ObservableObject {
         allKeys.append(key)
     }
     
-    init(){
-        allItems["1"]=ToDoItem(titleText: "Grocery", descriptionText: "Nothn", date: "2022-12-1")
-        allItems["2"]=ToDoItem(titleText: "Study", descriptionText: "I have to study this and that I have to study this and that I have to study this and that I have to study this and that I have to study this and that", date: "2022-12-1")
-        allItems["3"]=ToDoItem(titleText: "Cleaning", descriptionText: "Nothn", date: "2022-12-1")
-        allKeys.append("1")
-        allKeys.append("2")
-        allKeys.append("3")
-    }
+//    init(){
+//        allItems["1"]=ToDoItem(titleText: "1.Grocery", descriptionText: "Nothn", date: "2022-12-1")
+//        allItems["2"]=ToDoItem(titleText: "2.Study", descriptionText: "I have to study this and that I have to study this and that I have to study this and that I have to study this and that I have to study this and that", date: "2022-12-1")
+//        allItems["3"]=ToDoItem(titleText: "3.Cleaning", descriptionText: "Nothn", date: "2022-12-1")
+//        allKeys.append("1")
+//        allKeys.append("2")
+//        allKeys.append("3")
+//    }
 
     /// Deletes an item from allItems database
     /// - Parameter index: an IndexSet
