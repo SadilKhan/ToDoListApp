e78981b990a32a782ec7079690253f95083f2c71
SadilKhan <edwardstephendrood@gmail.com>
2022-10-31 23:00:53 +0100

    Version 2.2
    1. Added Search button
    2. Fixed a bug when an item if it's toggled on and then off and after the updated button is clicked, it would be removed.

--- ToDoListApp/ContentView.swift
+++ ToDoListApp/ContentView.swift
@@ -7,6 +7,7 @@ struct ContentView: View {
     let navTitle: String = "Todos"
     @StateObject var itemDataBase: ItemDB = ItemDB()
     @State var toUpdate: Bool = false
+    @State var searchText: String = ""
     //@State var isUndoDisabled:Bool
     var body: some View {
         NavigationView { // NAV START
@@ -15,17 +16,18 @@ struct ContentView: View {
                     .font(.title2)
                     .fontWeight(.light)
                 List { // LIST START
-                    ForEach(itemDataBase.allKeys, id: \.self) { key in
+                    ForEach(searchResults, id: \.self) { key in
                         NextPageNavLink(key: key)
                     }
                         .onDelete(perform: itemDataBase.deleteItem)
                         .onChange(of: toUpdate) { newValue in
-                            itemDataBase.deleteItemDone()
+                        itemDataBase.deleteItemDone()
                     }
                 } // LIST END
                 .listStyle(.grouped)
                 // Navigation Bar Customization
-                .navigationTitle(navTitle)
+                .searchable(text: $searchText)
+                    .navigationTitle(navTitle)
                     .toolbar {
                     // Edit Button
                     ToolbarItem(placement: .navigationBarLeading) {
@@ -55,133 +57,147 @@ struct ContentView: View {
 //    init(){
 //        self._isUndoDisabled = State(initialValue: true)
 //    }
-    
-}
+
+    var searchResults: [String] {
+        if searchText.isEmpty {
+            return itemDataBase.allKeys
+        } else {
+            var selectedKeys: [String] = []
+            for (key, value) in itemDataBase.allItems {
+                if value.getTitleText().lowercased().contains(searchText.lowercased()) || value.getDecriptionText().lowercased().contains(searchText.lowercased()) {
+                    selectedKeys.append(key)
+                    }
+                }
+            return selectedKeys
+            }
+        }
+
+    }
 
 // MARK: NAVIGATION PAGE LINK
-struct NextPageNavLink: View {
-    var key: String
-    @EnvironmentObject var itemDataBase: ItemDB
-    var body: some View {
-        NavigationLink {
-            if let item = itemDataBase.allItems[key] {
-                InformationView(key: key, item: item)
+    struct NextPageNavLink: View {
+        var key: String
+        @EnvironmentObject var itemDataBase: ItemDB
+        var body: some View {
+            NavigationLink {
+                if let item = itemDataBase.allItems[key] {
+                    InformationView(key: key, item: item)
+                }
+            } label: {
+                ItemList(key: key)
             }
-        } label: {
-            ItemList(key: key)
         }
     }
-}
 
 // MARK: NEW BUTTON
 /// New Button which opens InformationView Window
-struct NewButton: View {
-    var body: some View {
-        NavigationLink {
-            InformationView()
-        } label: {
-            Image(systemName: "plus")
-        }
+    struct NewButton: View {
+        var body: some View {
+            NavigationLink {
+                InformationView()
+            } label: {
+                Image(systemName: "plus")
+            }
 
+        }
     }
-}
 
 // MARK: NEW BUTTON
 /// New Button which opens InformationView Window
-struct HelpButton: View {
-    var body: some View {
-        Link("Help", destination: URL(string: "https://github.com/SadilKhan/ToDoListApp")!)
+    struct HelpButton: View {
+        var body: some View {
+            Link("Help", destination: URL(string: "https://github.com/SadilKhan/ToDoListApp")!)
+        }
     }
-}
 
 // MARK: ADD BUTTON
 /// Opens a new window for adding an item
-struct AddButton: View {
-    var body: some View {
-        NavigationLink {
-            InformationView()
-        } label: {
-            Image(systemName: "plus")
-        }
-    }
-}
-
-struct UndoButton:View{
-//    @Binding var isUndoDisabled: Bool
-    @EnvironmentObject var itemDataBase: ItemDB
-    var body: some View {
-        Button {
-            if itemDataBase.allDeletedItems.count>0{
-                itemDataBase.appendItem(itemDataBase.allDeletedItems[0].1)
-                itemDataBase.allDeletedItems.removeFirst()
+    struct AddButton: View {
+        var body: some View {
+            NavigationLink {
+                InformationView()
+            } label: {
+                Image(systemName: "plus")
             }
-        } label: {
-            Text("Undo")
         }
     }
-}
+
+    struct UndoButton: View {
+//    @Binding var isUndoDisabled: Bool
+        @EnvironmentObject var itemDataBase: ItemDB
+        var body: some View {
+            Button {
+                if itemDataBase.allDeletedItems.count > 0 {
+                    itemDataBase.appendItem(itemDataBase.allDeletedItems[0].1)
+                    itemDataBase.allDeletedItems.removeFirst()
+                }
+            } label: {
+                Text("Undo")
+            }
+        }
+    }
 
 // MARK: UPDATE BUTTON
 /// Updates the current list based on the completed item
-struct UpdateButton: View {
-    @EnvironmentObject var itemDataBase: ItemDB
-    @Binding var toUpdate: Bool
-    var body: some View {
-        Button {
-            toUpdate.toggle()
-        } label: {
-            Text("Update")
-        }
+    struct UpdateButton: View {
+        @EnvironmentObject var itemDataBase: ItemDB
+        @Binding var toUpdate: Bool
+        var body: some View {
+            Button {
+                toUpdate.toggle()
+            } label: {
+                Text("Update")
+            }
 
+        }
     }
-}
 
 // MARK: ITEM LIST
 
 /// The item lists that will be shown on the main page
 /// - Parameters
 ///    - key: String
-struct ItemList: View {
-    var key: String
-    @EnvironmentObject var itemDataBase: ItemDB
-    @State var isDone: Bool = false
-    var body: some View {
-        HStack {
-            Circle()
-                .fill(itemDataBase.allColors[key] != nil ? itemDataBase.allColors[key]! : Color.primary)
-                .frame(width: 35, height: 35)
-            Toggle(isOn: $isDone) {
-                VStack(alignment: .leading) {
-                    Text(itemDataBase.allItems[key] != nil ? itemDataBase.allItems[key]!.getTitleText() : "")
-                        .font(.body)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.primary)
-                    Text(itemDataBase.allItems[key] != nil ? itemDataBase.allItems[key]!.getDecriptionText() : "")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .frame(maxHeight: 10, alignment: .leading)
+    struct ItemList: View {
+        var key: String
+        @EnvironmentObject var itemDataBase: ItemDB
+        @State var isDone: Bool = false
+        var body: some View {
+            HStack {
+                Circle()
+                    .fill(itemDataBase.allColors[key] != nil ? itemDataBase.allColors[key]! : Color.primary)
+                    .frame(width: 35, height: 35)
+                Toggle(isOn: $isDone) {
+                    VStack(alignment: .leading) {
+                        Text(itemDataBase.allItems[key] != nil ? itemDataBase.allItems[key]!.getTitleText() : "")
+                            .font(.body)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.primary)
+                        Text(itemDataBase.allItems[key] != nil ? itemDataBase.allItems[key]!.getDecriptionText() : "")
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                            .frame(maxHeight: 10, alignment: .leading)
+                    }
+                }
+                    .onChange(of: isDone) { newValue in
+                    itemDataBase.allDone[key] = newValue
                 }
             }
-                .onChange(of: isDone) { newValue in
-                itemDataBase.allDone[key] = true
-            }
-        }
 //            .padding(.vertical, 10)
 //        .padding(10)
 //        .background(ColorsDB().bgColorItemList.opacity(0.1).cornerRadius(20))
 //            .padding(.horizontal,10)
+        }
+
+        init(key: String) {
+            self.key = key
+        }
+
     }
 
-    init(key: String) {
-        self.key = key
+
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-
-}
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
