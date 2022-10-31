77fddc0cd18883265f398944e0df03d435db0545
SadilKhan <edwardstephendrood@gmail.com>
2022-10-31 07:26:58 +0100

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Md. Sadil Khan on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
